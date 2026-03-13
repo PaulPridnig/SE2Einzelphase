9f8e072b0489f5e83749a543a8b9ea901b6d551e
Paul <pridnigp@msn.com>
2026-03-13 09:32:03 +0100

    All Tests implemented (2.2.3) and final adjustments

--- src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
-import kotlin.unaryMinus
 
 @RestController
 @RequestMapping("/leaderboard")
